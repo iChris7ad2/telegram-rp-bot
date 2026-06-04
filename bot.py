4403a9a2df61c612b656fbc14dbce1984e511e59
iChris7ad2 <christian.wagner2004@gmail.com>
2026-06-04 09:17:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,34 @@ import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+konten = {}
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot läuft auf Railway!")
+    user = update.effective_user.id
+
+    if user not in konten:
+        konten[user] = 1000
+
+    await update.message.reply_text(
+        "🏙️ Willkommen im RP!\n"
+        "Du hast 1000 € Startgeld bekommen.\n"
+        "Schreibe /konto"
+    )
+
+async def konto(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user.id
+
+    if user not in konten:
+        konten[user] = 1000
+
+    await update.message.reply_text(
+        f"💳 Dein Kontostand: {konten[user]} €"
+    )
 
 app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
 
-print("Bot gestartet")
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("konto", konto))
+
+print("Bot läuft...")
 app.run_polling()
